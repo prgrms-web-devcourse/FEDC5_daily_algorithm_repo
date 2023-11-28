ef8579b64567af156147cb6d375d98ad2fb06a47
jonguk <sinjuk1@naver.com>
2023-11-28 17:14:42 +0900

    solved: 신종욱 / 알고리즘 / 10.24~11.28

---
+++ 신종욱/기상캐스터/기상캐스터.js
@@ -0,0 +1,29 @@
+const input = require("fs")
+    .readFileSync("/dev/stdin")
+    .toString()
+    .trim()
+    .split("\n");
+
+const [h, w] = input[0].split(" ").map(Number);
+input.splice(0, 1);
+
+const graph = input.map((row) => row.split(""));
+const result = Array.from(
+    { length: h },
+    () => Array.from({ length: w }, () => -1)
+    // 전부 -1 로 미리 초기화
+);
+
+graph.forEach((row, ridx) =>
+    row.forEach((value, cidx) => {
+        if (value === "c") {
+            result[ridx][cidx] = 0;
+        } else if (value === ".") {
+            if (cidx - 1 >= 0 && result[ridx][cidx - 1] !== -1) {
+                result[ridx][cidx] = result[ridx][cidx - 1] + 1;
+            }
+        }
+    })
+);
+
+console.log(result.map((row) => row.join(" ")).join("\n"));
